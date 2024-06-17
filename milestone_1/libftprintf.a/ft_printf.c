8d13174ad2436fb7f3616843513b1c20b06b02dc
root <root@hello-world>
2024-06-18 00:02:05 +0300

    Upload

--- milestone_1/libftprintf.a/ft_printf.c
+++ milestone_1/libftprintf.a/ft_printf.c
@@ -2,8 +2,22 @@
 
 int ft_printf(const char *hello, ...)
 {
+    char *ch;
+
+    ch = (char *)hello;
     va_list arg;
     va_start(arg,hello);
-    check_arg(arg,(char *)hello);
+    while(*ch)
+    {
+        if(*ch == '%' && *(++ch) && *(ch) != '%')
+        {
+            check_arg(arg,ch);
+        }
+        else
+        {
+            ft_putchar_fd(*ch, 1);
+        }
+        ch++;
+    }
     va_end(arg);
 }
