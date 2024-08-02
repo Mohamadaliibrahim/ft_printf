877d2cdf230a7a1aa953c2ff148de3acc77dc8de
Mohammad Ali Ibrahim <mohamibr@c1r3s3.42beirut.com>
2024-08-02 21:43:58 +0300

    Upload

---
+++ ymkn/stack2.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohamibr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 20:05:47 by mohamibr          #+#    #+#             */
+/*   Updated: 2024/08/02 20:05:51 by mohamibr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	pa(t_stack **stack_a, t_stack **stack_b)
+{
+	if (push(stack_a, stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("pa", 1);
+	return (0);
+}
+
+int	pb(t_stack **stack_a, t_stack **stack_b)
+{
+	if (push(stack_b, stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("pb", 1);
+	return (0);
+}
+
+int	rotate(t_stack **stack)
+{
+	t_stack	*head;
+	t_stack	*tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	*stack = head->next;
+	head->next = NULL;
+	tail->next = head;
+	return (0);
+}
+
+int	ra(t_stack **stack_a)
+{
+	if (rotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("ra", 1);
+	return (0);
+}
+
+int	rb(t_stack **stack_b)
+{
+	if (rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rb", 1);
+	return (0);
+}
+//stop!!
---
+++ ymkn/stack9.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohamibr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 20:05:47 by mohamibr          #+#    #+#             */
+/*   Updated: 2024/08/02 20:05:51 by mohamibr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	printlist(t_stack *head)
+{
+	t_stack	*tmp;
+
+	tmp = head;
+	while (tmp != NULL)
+	{
+		ft_putnbr_fd(tmp->data, 1);
+		ft_putendl_fd("", 1);
+		tmp = tmp->next;
+	}
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_stack	*head;
+	t_stack	*tmp;
+
+	head = *stack;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(stack);
+}
+
+void	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free (split[i]);
+		i++;
+	}
+	free (split);
+}
